f33ece3778cf34984a516a6ae641b9cd9dd03286
Ashley Jelks <ashley@truss.works>
2015-12-31 11:34:59 -0800

    first commit of test code for creating and working with segues in Swift

---
+++ Segues Example/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Segues Example
+//
+//  Created by Ashley Jelks on 12/29/15.
+//  Copyright © 2015 Ashley Jelks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
